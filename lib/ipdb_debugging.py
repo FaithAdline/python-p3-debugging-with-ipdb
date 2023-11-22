62146ce717d0c186ffac0ef7b513ca6a0730bdbf
Faith/Adline <adlinefaith@gmail.com>
2023-11-22 17:40:17 +0300

    completed

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,15 @@
+
 #!/usr/bin/env python3
 
 import ipdb
 
 def plus_two(num):
-    num + 2
+   
+    num = num + 2     # Update num by adding 2
     return num
+
+# Call the function with an example value
+result = plus_two(3)
+print(result)  # Print the result to verify the fix
+
+
